4b87775aa857d6b7dd1389062a84b52010fb8f3a
RaylenZed <raylenzed@gmail.com>
2026-02-15 02:49:28 +0800

    qB: enforce WebUI prefs block under [Preferences]

--- qbittorrent/init/10-reverse-proxy.sh
+++ qbittorrent/init/10-reverse-proxy.sh
@@ -10,27 +10,38 @@ if [[ ! -f "${CONF}" ]]; then
 INI
 fi
 
-if ! grep -q '^\[Preferences\]$' "${CONF}"; then
-  printf '\n[Preferences]\n' >> "${CONF}"
-fi
+# Clear any previously injected block and legacy WebUI keys (both old and new formats).
+sed -i \
+  -e '/^; ARKOS_WEBUI_BEGIN$/,/^; ARKOS_WEBUI_END$/d' \
+  -e '/^WebUIAddress=/d' \
+  -e '/^WebUIPort=/d' \
+  -e '/^WebUIHostHeaderValidation=/d' \
+  -e '/^WebUICSRFProtection=/d' \
+  -e '/^WebUIReverseProxySupportEnabled=/d' \
+  -e '/^WebUIServerDomains=/d' \
+  -e '/^WebUITrustedReverseProxies=/d' \
+  -e '/^WebUIAlternativeUIEnabled=/d' \
+  -e '/^WebUI\\Address=/d' \
+  -e '/^WebUI\\Port=/d' \
+  -e '/^WebUI\\HostHeaderValidation=/d' \
+  -e '/^WebUI\\CSRFProtection=/d' \
+  -e '/^WebUI\\ReverseProxySupportEnabled=/d' \
+  -e '/^WebUI\\ServerDomains=/d' \
+  -e '/^WebUI\\TrustedReverseProxies=/d' \
+  -e '/^WebUI\\AlternativeUIEnabled=/d' \
+  "${CONF}"
 
-set_pref() {
-  local key="$1"
-  local value="$2"
-  local key_pattern
-  key_pattern="$(printf '%s' "${key}" | sed -e 's/[][\\/.*^$]/\\&/g')"
-  if grep -Fq "${key}=" "${CONF}"; then
-    sed -i "s|^${key_pattern}=.*|${key}=${value}|" "${CONF}"
-  else
-    printf '%s=%s\n' "${key}" "${value}" >> "${CONF}"
-  fi
-}
-
-set_pref 'WebUI\Address' '0.0.0.0'
-set_pref 'WebUI\Port' '8080'
-set_pref 'WebUI\HostHeaderValidation' 'false'
-set_pref 'WebUI\CSRFProtection' 'false'
-set_pref 'WebUI\ReverseProxySupportEnabled' 'false'
-set_pref 'WebUI\ServerDomains' '*'
-set_pref 'WebUI\TrustedReverseProxies' '127.0.0.1/8;10.0.0.0/8;172.16.0.0/12;192.168.0.0/16;fc00::/7'
-set_pref 'WebUI\AlternativeUIEnabled' 'false'
+# Append an explicit Preferences block at EOF so keys never end up under a wrong section.
+cat >> "${CONF}" <<'EOF'
+; ARKOS_WEBUI_BEGIN
+[Preferences]
+WebUI\Address=0.0.0.0
+WebUI\Port=8080
+WebUI\HostHeaderValidation=false
+WebUI\CSRFProtection=false
+WebUI\ReverseProxySupportEnabled=false
+WebUI\ServerDomains=*
+WebUI\TrustedReverseProxies=127.0.0.1/8;10.0.0.0/8;172.16.0.0/12;192.168.0.0/16;fc00::/7
+WebUI\AlternativeUIEnabled=false
+; ARKOS_WEBUI_END
+EOF
